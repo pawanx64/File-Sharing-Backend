fd00d706c60b8c283aaf83bc0b8f28ebc317aaf5
pawanx64 <pawankumar.nov5@gmail.com>
2025-08-05 21:15:42 +0530

    Initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 app.use(
     cors({
-        origin:["http://localhost:3000"],
+        origin:["http://localhost:3000", "https://skyboxshare.vercel.app"],
         methods:["POST","GET", "DELETE"], // Added DELETE method for file deletion https://skyboxshare.vercel.app
         credentials: true,
     })
